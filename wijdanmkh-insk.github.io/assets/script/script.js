9175df7f38828efd1bef68b3198d50f24b5c97d8
Dan <34543247+wijdanmkh-insk@users.noreply.github.com>
2024-08-19 20:56:22 +0700

    Add files via upload

---
+++ wijdanmkh-insk.github.io/assets/script/script.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Toggle Navbar Menu
+    function toggle() {
+        const navToggle = document.getElementById('nav-toggle');
+        const navbar = document.querySelector('.nav ul');
+
+        navToggle.addEventListener('click', () => {
+            navbar.classList.toggle('show');
+        });
+    }
+    window.onload = toggle;
+
+    //Change Image
+    function frown() {
+        const frown = document.getElementById('me-frown');
+        const hoverchange = document.getElementById('div');
+        const quarterCircle = document.getElementById('quarter-circle');
+        const rootStyles = getComputedStyle(document.documentElement);
+
+        hoverchange.addEventListener('mouseenter', () => {
+            frown.style.opacity = 0;
+            setTimeout(() => {
+                frown.src = './assets/img/me.png';
+                frown.style.opacity = 1;
+            }, 500);
+
+            quarterCircle.style.backgroundColor = rootStyles.getPropertyValue('--a4');
+        });
+
+        hoverchange.addEventListener('mouseleave', () => {
+            frown.style.opacity = 0;
+            setTimeout(() => {
+                frown.src = './assets/img/me-frown.png';
+                frown.style.opacity = 1;
+            }, 500);
+
+            quarterCircle.style.backgroundColor = rootStyles.getPropertyValue('grey');
+        });
+    }
+
+    toggle();
+    frown();
+});
