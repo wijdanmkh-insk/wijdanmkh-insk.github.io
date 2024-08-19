ee3dedd27aebcd649702663cd30e1803d3904153
Dan <34543247+wijdanmkh-insk@users.noreply.github.com>
2024-08-19 11:22:14 +0700

    Delete assets directory

--- assets/script/script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript to toggle the 'show' class
-document.addEventListener("DOMContentLoaded", function() {
-    const navToggle = document.querySelector(".nav-toggle");
-    const navMenu = document.querySelector(".nav ul");
-
-    navToggle.addEventListener("click", function() {
-        navMenu.classList.toggle("show");
-    });
-});
